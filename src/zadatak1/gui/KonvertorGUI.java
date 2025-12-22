4d1309544e5061707dee508b62e6e99a1823c784
bojantomic <tomicb@fon.rs>
2025-12-22 13:35:06 +0100

    Prepravljen GUI za IntelliJ Swing UI desiner

--- src/zadatak1/gui/KonvertorGUI.java
+++ src/zadatak1/gui/KonvertorGUI.java
@@ -1,129 +1,86 @@
 package zadatak1.gui;
 
-import java.awt.EventQueue;
+import zadatak1.poslovna_logika.Konvertor;
+
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
-import javax.swing.border.EmptyBorder;
-
-import zadatak1.poslovna_logika.Konvertor;
-
+// Klasa KonvertorGUI mora da nasledi JFrame da
+// bi predstavljala pravi prozor na koji moze da
+// se postavi panel koji se pravi u Swing UI grafickom editoru
 public class KonvertorGUI extends JFrame {
 
-	private static final long serialVersionUID = 1L;
+    //Svaka od komponenti sa panela, a i sam panel
+    //ce se ovde naci kao privatni atributi
+    private JTextField poljeDinari;
+    private JTextField poljeEvri;
+    private JButton konvertujDUEButton;
+    private JButton konvertujEUDButton;
+    private JPanel mojPanel;
 
-	private Konvertor konvertor = new Konvertor();
+    //Konstruktor u kojem postavljamo osnovne parametre
+    //samog prozora: dodavanje panela, naslov prozora,
+    //kako se zatvara, da li se moze smanjiti/povecati i td.
+    //Osim toga, tu su i event handler metode
+    public KonvertorGUI(){
+        //Postavljanje napravljenog panela na prozor
+        setContentPane(mojPanel);
 
-	private JPanel contentPane;
-	private JLabel lblIznosUDinarima;
-	private JLabel lblIznosUEvrima;
-	private JTextField dinariField;
-	private JTextField evriField;
-	private JButton btnKonvertujDue;
-	private JButton btnKonvertujEud;
+        //Postavljanje naslova prozora
+        setTitle("Konvertor");
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					KonvertorGUI frame = new KonvertorGUI();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+        //Kad se prozor zatvori, cela aplikacija se gasi
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-	/**
-	 * Create the frame.
-	 */
-	public KonvertorGUI() {
-		setTitle("Konvertor valuta");
-		setResizable(false);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 423, 220);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		contentPane.add(getLblIznosUDinarima());
-		contentPane.add(getLblIznosUEvrima());
-		contentPane.add(getDinariField());
-		contentPane.add(getEvriField());
-		contentPane.add(getBtnKonvertujDue());
-		contentPane.add(getBtnKonvertujEud());
-	}
+        //Prozor se ne moze smanjiti/povecati
+        setResizable(false);
 
-	private JLabel getLblIznosUDinarima() {
-		if (lblIznosUDinarima == null) {
-			lblIznosUDinarima = new JLabel("Iznos u dinarima:");
-			lblIznosUDinarima.setBounds(41, 47, 144, 14);
-		}
-		return lblIznosUDinarima;
-	}
+        //Podesavanje velicine prozora u pikselima
+        setSize(400, 200);
+        //Umesto setSize, moze pack koji pravi prozor
+        // u najmanjoj mogucoj dimenziji, a da se sve vidi
+        //pack();
 
-	private JLabel getLblIznosUEvrima() {
-		if (lblIznosUEvrima == null) {
-			lblIznosUEvrima = new JLabel("Iznos u evrima:");
-			lblIznosUEvrima.setBounds(232, 47, 144, 14);
-		}
-		return lblIznosUEvrima;
-	}
+        // Podesavanje GDE ce se prozor iscrtati u odnosu na ekran
+        // ovako ce se iscrtati na sredini
+        setLocationRelativeTo(null);
 
-	private JTextField getDinariField() {
-		if (dinariField == null) {
-			dinariField = new JTextField();
-			dinariField.setBounds(41, 79, 144, 20);
-			dinariField.setColumns(10);
-		}
-		return dinariField;
-	}
+        konvertujDUEButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Konvertor k = new Konvertor();
 
-	private JTextField getEvriField() {
-		if (evriField == null) {
-			evriField = new JTextField();
-			evriField.setBounds(232, 79, 144, 20);
-			evriField.setColumns(10);
-		}
-		return evriField;
-	}
+                //Preuzimanje dinara iz polja za unos dinara
+                // i pretvaranje iz String u double
+                double dinari = Double.parseDouble(poljeDinari.getText());
 
-	private JButton getBtnKonvertujDue() {
-		if (btnKonvertujDue == null) {
-			btnKonvertujDue = new JButton("Dinari u evre");
-			btnKonvertujDue.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-					double dinari = Double.parseDouble(dinariField.getText());
-					double evri = konvertor.konvertujDinareUEvre(dinari);
-					evriField.setText("" + evri);
-				}
-			});
-			btnKonvertujDue.setBounds(41, 127, 144, 23);
-		}
-		return btnKonvertujDue;
-	}
+                //Konverzija dinara u evre
+                double evri = k.konvertujDinareUEvre(dinari);
 
-	private JButton getBtnKonvertujEud() {
-		if (btnKonvertujEud == null) {
-			btnKonvertujEud = new JButton("Evri u dinare");
-			btnKonvertujEud.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-					double evri = Double.parseDouble(evriField.getText());
-					double dinari = konvertor.konvertujEvreUDinare(evri);
-					dinariField.setText("" + dinari);
-				}
-			});
-			btnKonvertujEud.setBounds(232, 127, 144, 23);
-		}
-		return btnKonvertujEud;
-	}
+                //Postavljanje vrednosti u evrima u polje za evre
+                poljeEvri.setText(evri + "");
+            }
+        });
+        konvertujEUDButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Konvertor k = new Konvertor();
+
+                //Preuzimanje iznosa u evrima iz polja za unos
+                // i pretvaranje iz String u double
+                double evri = Double.parseDouble(poljeEvri.getText());
+
+                //Konverzija iz evra u dinare
+                double dinari = k.konvertujEvreUDinare(evri);
+
+                //Postavljanje vrednosti u dinarima u polje za dinare
+                poljeDinari.setText(dinari + "");
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        new KonvertorGUI().setVisible(true);
+    }
 }
